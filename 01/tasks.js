2fa83708b7d89121c8dfe530ed61c33b6dd5df1b
Vladimir Zhuravlev <izhur27@gmail.com>
2017-11-28 01:04:08 +0300

    wow

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,10 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
-
+	let ints = string.match( /[\-\d]+/g ).map((value) => {
+		return +value;
+	});
+	return { max: Math.max.apply(Math, ints), min: Math.min.apply(Math, ints) };
 }
 
 /* ============================================= */
@@ -16,7 +19,9 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+	let count;
+	(x === 2) || (x === 1) ? count = 1 : count = fibonacciSimple(x-1) + fibonacciSimple(x-2);
+	return count;
 }
 
 /* ============================================= */
@@ -28,7 +33,16 @@ function fibonacciSimple(x) {
  * @return {number} число под номером х
  */
 function fibonacciWithCache(x) {
-  return x;
+	try {
+		if (x in data) {
+			return data.x;
+		}
+	} catch (e) {
+		data = {}
+	}
+	
+	data.x = fibonacciSimple(x);
+	return data.x;
 }
 
 /* ============================================= */
@@ -49,7 +63,21 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
+	let mat = [[]];
+	let n = 0, s="";
+	while (n <= max) {
+		let row = mat[mat.length - 1];
+		let str_n;
+		n > 9 ? str_n = String(n) : str_n = [" ", n].join("");
+		row.length < 4 ? mat[mat.length - 1].push(str_n) : mat.push([str_n]);
+		n += 1;
+	}
 
+	mat = mat.map((element) => {
+		return element.join(" ");
+	})
+
+	return mat.join("\n");
 }
 
 /* ============================================= */
@@ -61,6 +89,33 @@ function printNumbers(max, cols) {
  */
 function rle(input) {
 
+	input = input.split("");
+
+	let index = 0;
+	while (true) {
+		if (index+1 >= input.length) {
+			break;
+		}
+
+		let step = 1;
+		while (true) {
+			if (input[index+step] !== input[index]) {
+				if (step > 1) {
+					input.splice(index+1, step-1, step);
+					index += 1;
+				}
+				break;
+			} else {
+				step += 1
+			}
+		}
+
+		index += 1;
+		console.log(input);
+
+	}
+
+	return input.join("");
 }
 
 module.exports = {
